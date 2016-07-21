a414704d8a03b334aa0267ac0db64b973ded1e55
Timothy Callahan <tim.p.callahan@gmail.com>
2016-07-21 10:39:23 -0400

    require user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,6 @@ class ApplicationController < ActionController::Base
   helper_method :logged_in?
 
   private
-  def require_no_user!
-    redirect_to cats_url if current_user
-  end
 
   def current_user
     return nil unless session[:session_token]
